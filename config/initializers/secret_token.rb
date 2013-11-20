239014941fbcf013d51e59908e3b0fee73081f24
Derek Savitzky <dereksavitzky@gmail.com>
2013-11-20 11:37:27 -0600

    First save for the Customer Relations Manager Challenge

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CRM::Application.config.secret_token = '6d345e26daa457e3400e8b4478a8a81ea40f3a821dbcb278c2d1491b2817b788cfd7997c460df27342ef894307e9e03ee1d2c675e1bbc4e8978503de82d80b3f'
